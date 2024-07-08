22432a80c49ebc4c89e2d5b411bd93dd9b1a6b3a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-07-08 09:18:52 +0300

    Calculator

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -6,3 +6,6 @@ import math
 st.title("Mortgage Repayment Calculator")
 
 st.write('### Input Data')
+col1, col2 = st.columns(2)
+home_value = col1.number_input("Home Value", min_value=0, max_value=500000)
+deposit
