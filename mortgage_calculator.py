b90468b259e414b43f718c6bde077ca65ed7d285
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-07-08 09:27:10 +0300

    Calculator

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -16,3 +16,9 @@ loan_term = col2.number_input("Loan Term (in years)", min_value=1,value=30)
 loan_amount = home_value - deposit
 monthly_interest_rate = (interest_rate / 100) / 12
 number_of_payments = loan_term * 12
+monthly_payment = (
+    loan_amount
+    * (monthly_interest_rate * (1 + monthly_interest_rate) ** number_of_payments)
+    / ((1 + monthly_interest_rate) ** number_of_payments - 1)
+)
+
