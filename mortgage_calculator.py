987ec162261e8d425e5ec87fd1d3643f19405898
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-07-08 09:21:48 +0300

    Calculator

--- mortgage_calculator.py
+++ mortgage_calculator.py
@@ -7,7 +7,7 @@ st.title("Mortgage Repayment Calculator")
 
 st.write('### Input Data')
 col1, col2 = st.columns(2)
-home_value = col1.number_input("Home Value", min_value=0, max_value=500000)
+home_value = col1.number_input("Home Value", min_value=0, value=500000)
 deposit = col1.number_input("Deposit", min_value=0, max_value=100000)
 interest_rate = col2.number_input("Interest  Rate (in %)", min_value=1, value=30)
 loan_term = col2.number_input("Loan Term (in years)", min_value=1,value=30)
